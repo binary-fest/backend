39e445070fc3289a162ae977c76ad8318ac947e6
AriefRahmanW <rivurjr@gmail.com>
2021-02-03 01:44:31 +0700

    update ormconfig

---
+++ ormconfig.js
@@ -0,0 +1,21 @@
+console.log("process.env.DATABASE_URL :>> ", process.env.DATABASE_URL)
+module.exports = {
+   "type": "postgres",
+   "url": process.env.DATABASE_URL,
+   "synchronize": true,
+   "logging": false,
+   "entities": [
+      "build/entity/**/*.ts"
+   ],
+   "migrations": [
+      "build/migration/**/*.ts"
+   ],
+   "subscribers": [
+      "build/subscriber/**/*.ts"
+   ],
+   "cli": {
+      "entitiesDir": "src/entity",
+      "migrationsDir": "src/migration",
+      "subscribersDir": "src/subscriber"
+   }
+}
