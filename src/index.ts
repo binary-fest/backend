80cc8a7192ff86f45516548acb43ebecfc7db866
AriefRahmanW <rivurjr@gmail.com>
2021-02-02 19:05:57 +0700

    fix error

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ createConnection().then(async connection => {
     // ...
 
     // start express server
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
 
     console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
 
