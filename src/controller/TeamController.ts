af9109c9328f9193ee23eb58e1f91122c8983193
Bagus TRIANURDIN <btstargroup@gmail.com>
2021-02-15 18:17:08 +0700

    Fix duplicate data in TeamController when saving data

--- src/controller/TeamController.ts
+++ src/controller/TeamController.ts
@@ -1,89 +1,71 @@
 import {NextFunction, Request, Response} from "express";
-import * as cloudinary from 'cloudinary'
 import { getRepository } from "typeorm";
 import { Team } from "../entity/team/Team";
 import { TeamMember } from "../entity/team/TeamMember";
 import { ReqTeamRegister } from "../model/ReqTeamRegister";
 
-const cloudinaryV2 = cloudinary.v2
-
-
 export class TeamController{
 
-    private teamRepository = getRepository(Team)
+  private teamRepository = getRepository(Team)
 
-    private teamMemberRepository = getRepository(TeamMember)
+  private teamMemberRepository = getRepository(TeamMember)
 
-    async register(req: Request, res: Response){
-        
-        const { team, members } = req.body as ReqTeamRegister
+  async register(req: Request, res: Response){
+      
+    const { team, members } = req.body as ReqTeamRegister
 
-        if(team === undefined || (members === undefined)){
-            res.status(400).json({
-                message: "Request Not Valid"
-            })
+    if(team === undefined || (members === undefined)){
+      res.status(400).json({
+        message: "Request Not Valid"
+      })
 
-            return;
-        }
+      return;
+    }
 
-        // validation
-        let counter = 0
-        for(let i=0; i < members.length; i++ ){
-            if(members[i].isLeader){
-                counter++
-            }   
-        }
+    // validation
+    let counter = 0
+    for(let i=0; i < members.length; i++ ){
+      if(members[i].isLeader){
+        counter++
+      }   
+    }
 
-        if(counter !== 1){
-            res.status(400).json({
-                message: "Leader must be 1"
-            })
-            return;
-        }
+    if(counter !== 1){
+      res.status(400).json({
+          message: "Leader must be 1"
+      })
+      return;
+    }
 
-        // let mTeam = new Team()
-        // mTeam = team
+    await this.teamRepository
+      .save(team)
+      .then(async() => {
 
-        // const teamMembers = new Array<TeamMember>(members.length)
+        for(let i=0; i < members.length; i++){
+          members[i].team = team
 
-        await this.teamRepository
-            .save(team)
-            .then(async() => {
-                this.teamRepository.save(team)
-        
-                    for(let i=0; i < members.length; i++){
-                        members[i].team = team
-
-                        await this.teamMemberRepository
-                            .save(members[i])
-                            .then(() => {})
-                            .catch((error) => {
-                                res.status(500).json({
-                                    message: error.message,
-                                    error
-                                })
-                            })
-                    }
-
-                    // success
-                    res.status(200).json({
-                        message: "success add new team"
-                    })
-            })
+          await this.teamMemberRepository
+            .save(members[i])
+            .then(() => {})
             .catch((error) => {
-                res.status(500).json({
-                    message: error.message,
-                    error
-                })
+              res.status(500).json({
+                  message: error.message,
+                  error
+              })
             })
+        }
 
-    }
-
-    async registerUiux(req: Request){
-        
-
-    }
-
-    
+        // success
+        res.status(200).json({
+            message: "success add new team"
+        })
+      })
+      .catch((error) => {
+        res.status(500).json({
+            message: error.message,
+            error
+        })
+      })
+  }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import {Request, Response} from "express";
 import * as helmet from "helmet";
 import * as cors from "cors";
 import * as compression from "compression"
-import * as cloudinary from 'cloudinary'
 import * as rateLimit from 'express-rate-limit'
 import * as dotenv from 'dotenv'
 import {Routes} from "./routes";
@@ -14,48 +13,44 @@ import config from "./config/config";
 
 dotenv.config()
 
-const cloudinaryV2 = cloudinary.v2
-
-cloudinaryV2.config(config.cloudinary)
-
 createConnection().then(async connection => {
-    // create express app
-    const app = express();
-    app.use(cors({
-        origin: process.env.NODE_ENV === 'pruduction' ? "*" : "*"
-    }));
-    app.use(compression())
-    app.use(helmet());
-    app.use(bodyParser.json({limit: "5mb"}));
-    app.use(rateLimit({
-        windowMs: 1 * 60 * 1000,
-        max: 10
-    }))
+  // create express app
+  const app = express();
+  app.use(cors({
+    origin: process.env.NODE_ENV === 'pruduction' ? "*" : "*"
+  }));
+  app.use(compression())
+  app.use(helmet());
+  app.use(bodyParser.json({limit: "5mb"}));
+  app.use(rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 10
+  }))
 
-    // register express routes from defined application routes
-    const nextFunc = (req: Request, res: Response, next: Function) => {next()}
-    Routes.forEach(route => {
-        (app as any)[route.method](
-            route.route, 
-            route.middleware !== undefined ? route.middleware : nextFunc, 
-            (req: Request, res: Response, next: Function) => {
-                const result = (new (route.controller as any))[route.action](req, res, next);
-                if (result instanceof Promise) {
-                    result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+  // register express routes from defined application routes
+  const nextFunc = (req: Request, res: Response, next: Function) => {next()}
+  Routes.forEach(route => {
+    (app as any)[route.method](
+      route.route, 
+      route.middleware !== undefined ? route.middleware : nextFunc, 
+      (req: Request, res: Response, next: Function) => {
+        const result = (new (route.controller as any))[route.action](req, res, next);
+        if (result instanceof Promise) {
+          result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
 
-                } else if (result !== null && result !== undefined) {
-                    res.json(result);
-                }
-            }
-        );
-    });
+        } else if (result !== null && result !== undefined) {
+          res.json(result);
+        }
+      }
+    );
+  });
 
-    // setup express app here
-    // ...
+  // setup express app here
+  // ...
 
-    // start express server
-    app.listen(process.env.PORT || 3000);
+  // start express server
+  app.listen(process.env.PORT || 3000);
 
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
+  console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
 
 }).catch(error => console.log(error));
