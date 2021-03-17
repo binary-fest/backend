5641769e88d06594c2341a187ec568bbd26717d0
Bagus TRIANURDIN <btstargroup@gmail.com>
2021-03-17 22:17:40 +0700

    adding TeamSubmission entity database

---
+++ src/entity/team/TeamSubmission.ts
@@ -0,0 +1,33 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+export enum SubmissionStatus{
+  pending = "pending",
+  approved = "approved",
+  rejected = "rejected"
+}
+
+@Entity()
+export class TeamSubmission {
+
+  @PrimaryGeneratedColumn()
+  id_team_submission: number
+
+  @Column()
+  submission_type: number
+
+  @Column()
+  submission_name: string
+
+  @Column()
+  url_files: string
+
+  @Column()
+  status: SubmissionStatus
+
+  @Column({ default: () => "CURRENT_TIMESTAMP" })
+  createdAt: Date
+
+  @Column({ default: () => "CURRENT_TIMESTAMP" })
+  updatedAt: Date
+
+}
