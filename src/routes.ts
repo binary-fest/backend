7db8e7fa8c71557a095d3e382448c9f54e96ba53
Bagus TRIANURDIN <btstargroup@gmail.com>
2021-03-23 19:11:40 +0700

    Remove unnecessary code

--- src/routes.ts
+++ src/routes.ts
@@ -36,26 +36,5 @@ export const Routes = [
         route: "/api/competition/register",
         controller: TeamController,
         action: "register"
-    },
-    {
-        method: "put",
-        route: "/api/team/status",
-        controller: TeamController,
-        action: "status",
-        middleware: [checkJwt, checkRole(['uiux', 'iot'])]
-    },
-    {
-        method: "get",
-        route: "/api/team/members",
-        controller: TeamController,
-        action: "allMembers",
-        middleware: [checkJwt, checkRole(['uiux', 'iot'])]
-    },
-    {
-        method: "get",
-        route: "/api/team/submissions",
-        controller: TeamController,
-        action: "allSubmissions",
-        middleware: [checkJwt, checkRole(['uiux', 'iot'])]
     }
 ];
